2fb6d4a340a9418d57d8588a29d2e67921f45c13
JorgeAjs02 <jajgjorge@gmail.com>
2021-07-14 14:14:24 -0400

    casi terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import reactDom from 'react-dom';
+// import './App.css';
+import Formulario from "./Components/Formulario.js"
+
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+      type: 'dark',
+  },
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Formulario/>
+    </ThemeProvider>
   );
 }
-
-export default App;
+export default App; 
