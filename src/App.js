4185c09dcbb8864138f05599a536fcbd5aed6ab6
imanolarza <imanolarza@gmail.com>
2021-07-23 09:50:43 -0400

    nueva estructura

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import reactDom from 'react-dom';
 // import './App.css';
-import Formulario from "./Components/Formulario.js"
+import Formulario from "./Components/Formulario/index.js"
 
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
